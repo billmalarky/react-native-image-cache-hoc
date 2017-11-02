c81cd532e031e2db1842128c8e6fd4e7e96f178f
Reid Mayo <reidmayo@gmail.com>
2017-11-02 16:51:43 -0400

    Extend pruneCache test coverage

--- tests/config.js
+++ tests/config.js
@@ -35,7 +35,7 @@ jest.mock('react-native-fetch-blob', () => {
 
         let lstat = [
           {'size':'43663','path':mockData.basePath + '/cache/0fbbfec764c73ee5b4e3a0cb8861469bc9fc6c8c.jpg','filename':'0fbbfec764c73ee5b4e3a0cb8861469bc9fc6c8c.jpg','lastModified':1508878829000,'type':'file'},
-          {'size':'88937','path':mockData.basePath + '/cache/6865fd0a65771b0044319f562873cc7b145abb4a.jpg','filename':'6865fd0a65771b0044319f562873cc7b145abb4a.jpg','lastModified':1508877930000,'type':'file'},
+          {'size':'9000000','path':mockData.basePath + '/cache/6865fd0a65771b0044319f562873cc7b145abb4a.jpg','filename':'6865fd0a65771b0044319f562873cc7b145abb4a.jpg','lastModified':1508877930000,'type':'file'},
           {'size':'14133330','path':mockData.basePath + '/cache/b003269c377af6a2f53f59bc127a06c86f54312b.jpg','filename':'b003269c377af6a2f53f59bc127a06c86f54312b.jpg','lastModified':1508877698000,'type':'file'},
           {'size':'1684','path':mockData.basePath + '/cache/d1052b9f22c1f00f4d658224f4295307b97db69f.jpg','filename':'d1052b9f22c1f00f4d658224f4295307b97db69f.jpg','lastModified':1508877954000,'type':'file'},
           {'size':'65769','path':mockData.basePath + '/cache/faf4e58257d988ea6eab23aee5e5733bff9b2a9e.jpg','filename':'faf4e58257d988ea6eab23aee5e5733bff9b2a9e.jpg','lastModified':1509634852000,'type':'file'}
@@ -43,7 +43,8 @@ jest.mock('react-native-fetch-blob', () => {
 
         return lstat;
 
-      }
+      },
+      unlink: jest.fn()
     }
   };
 
